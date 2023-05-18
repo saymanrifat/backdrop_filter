8003ee3f5170f6a3e137a6d68dee9005022e9072
saymanrifat <98202630+saymanrifat@users.noreply.github.com>
2023-05-18 11:49:57 +0600

    V1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:io' show Platform, exit;
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -18,8 +21,47 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  void showAlertDialog() {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('Exit'),
+            content: Text('Are you want to close this app'),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    // Navigator.pop(context);
+                    if (Platform.isAndroid) {
+                      SystemNavigator.pop();
+                    } else if (Platform.isIOS) {
+                      exit(0);
+                    }
+                  },
+                  child: Text('Yes')),
+              TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text('No')),
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Blur Text - Backdrop Filter"),
+      ),
+      body: SafeArea(child: Container()),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showAlertDialog();
+        },
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
